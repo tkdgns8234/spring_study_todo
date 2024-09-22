12fa7e94686b46d69fcc2e7209d1ea3c900aa48d
tkdgns8234 <tkdgns82345@naver.com>
2024-09-22 23:34:08 +0900

    암/복호화 TODO

--- todo_demo_app/src/main/kotlin/com/example/tododemoapp/user/domain/User.kt
+++ todo_demo_app/src/main/kotlin/com/example/tododemoapp/user/domain/User.kt
@@ -10,6 +10,12 @@ import org.hibernate.annotations.BatchSize
  * JsonIdentityInfo: jackson 2.0 이후 신규 추가 (순환 참조 처리)
  **/
 
+/** TODO::User 정보 암호화하기 (password: 단방향 암호화, 그외 사용자 식별정보: 양방향 암호화)
+ * aws kms 를 통해 키 관리 및 암/복호화 알고리즘을 적용하여 보다 쉽게 데이터 암호화 처리 가능.
+ * aws kms <-> rds와의 연동 기능도 존재하는듯 보임
+ */
+
+
 @Entity
 @Table(name = "users")
 @JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator::class, property = "id")
